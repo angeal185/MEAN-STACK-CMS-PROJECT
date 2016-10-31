f43a3df90a11f1522f943cccde5c326afd6a71b8
angeal185 <beneaves01@hotmail.com>
2016-11-01 01:04:02 +0800

    commit

---
+++ initializers/templates/engine/templateEngineInitializer.js
@@ -0,0 +1,15 @@
+var ejsInitializer = require('../engines/ejsInitializer');
+var hbsInitializer = require('../engines/hbsInitializer');
+var jadeInitializer = require('../engines/jadeInitializer');
+var angularInitializer = require('../engines/angularInitializer');
+exports.initialize = function (__dirname, self, templateEngine) {
+    if (templateEngine !== undefined && templateEngine !== null) {
+        ejsInitializer.initialize(__dirname, self, templateEngine);
+        hbsInitializer.initialize(__dirname, self, templateEngine);
+        jadeInitializer.initialize(__dirname, self, templateEngine);
+		angularInitializer.initialize(__dirname, self, templateEngine);
+    }else{
+        hbsInitializer.initialize(__dirname, self, templateEngine);
+    }
+
+};
---
+++ initializers/templates/engines/angularInitializer.js
@@ -0,0 +1,8 @@
+﻿// initialize angular template engine
+exports.initialize = function (__dirname, self, templateEngine) {
+    if (templateEngine.engine === 'angular') {
+        self.app.set('view engine', 'angular');
+        self.app.set("views", __dirname + "/public");
+        console.log("Template Engine is: " + templateEngine.engine);
+    }
+};
