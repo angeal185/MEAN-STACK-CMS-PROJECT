db8996f89e38c230d34ecab94ddb42a8bfd15983
Angeal185 <beneaves01@hotmail.com>
2016-11-01 21:52:14 +0800

    Update templateEngineInitializer.js

--- initializers/templates/engine/templateEngineInitializer.js
+++ initializers/templates/engine/templateEngineInitializer.js
@@ -7,9 +7,19 @@ exports.initialize = function (__dirname, self, templateEngine) {
         ejsInitializer.initialize(__dirname, self, templateEngine);
         hbsInitializer.initialize(__dirname, self, templateEngine);
         jadeInitializer.initialize(__dirname, self, templateEngine);
-		angularInitializer.initialize(__dirname, self, templateEngine);
+	angularInitializer.initialize(__dirname, self, templateEngine);
     }else{
         hbsInitializer.initialize(__dirname, self, templateEngine);
     }
 
 };
+
+
+//todo 
+//fix hbs lag issue, dynamic loading issue, hbs-angular intergration
+
+//status
+//ejs/angular - working
+//jade/angular - working
+//angular/angular - working
+//handlebars/angular - fucked
